92591002349b07b0324e72c52fa110d176a7b6a0
Ruy Rocha <admin@ruyrocha.com>
2019-07-25 21:53:02 -0300

    Add serializers.

---
+++ app/serializers/dns_record_serializer.rb
@@ -0,0 +1,11 @@
+class DnsRecordSerializer < BaseSerializer
+  attribute :id
+
+  attribute :hostname do |_|
+    _.domain.name
+  end
+
+  attribute :ip_address do |_|
+    _.record.value
+  end
+end
