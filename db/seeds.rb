f2318b315fc3a863ba65807b57404f90c82f8a63
Ruy Rocha <admin@ruyrocha.com>
2019-07-30 20:27:58 -0300

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def assign_ip_to_domains_list(ip = '', domains = [])
+  if ip.presence && domains.presence
+    record = Record.create(value: ip, record_type: Record.record_types[:a])
+
+    domains.each do |hostname|
+      domain = Domain.create(name: hostname)
+      DnsRecord.create(domain: domain, record: record)
+    end
+    puts " --> Assigned #{ip} to #{domains.join(", ")}."
+  end
+end
+
+assign_ip_to_domains_list('1.1.1.1', %w(lorem.com ipsum.com dolor.com amet.com))
+assign_ip_to_domains_list('2.2.2.2', %w(ipsum.com))
+assign_ip_to_domains_list('3.3.3.3', %w(ipsum.com dolor.com amet.com))
+assign_ip_to_domains_list('4.4.4.4', %w(ipsum.com dolor.com sit.com amet.com))
+assign_ip_to_domains_list('5.5.5.5', %w(dolor.com sit.com))
